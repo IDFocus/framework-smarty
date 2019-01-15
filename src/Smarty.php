1ed8d1eb6629f5b5a138e109d0649e040f0e46bd
Sebastiaan Veldhuisen <sveldhuisen@idfocus.nl>
2019-01-15 22:48:06 +0100

    Append plugins dir (do not override vendor plugins dir)

--- src/Smarty.php
+++ src/Smarty.php
@@ -32,7 +32,7 @@ class Smarty {
 
 		$smarty->template_dir = realpath(dirname(__FILE__).'/../../../../templates');
 		$smarty->compile_dir = realpath(dirname(__FILE__).'/../../../../cache/templates_c');
-		$smarty->plugins_dir = realpath(dirname(__FILE__).'/../../../../plugins');
+		$smarty->plugins_dir[] = realpath(dirname(__FILE__).'/../../../../plugins');
 		$smarty->debugging = false;
 
 		if (defined('DEBUG') && DEBUG === true && defined('FORCE_COMPILE') && FORCE_COMPILE === true) {
